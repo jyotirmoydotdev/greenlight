8831aad3131e204022477a3fefb251957ce4d9f0
Jyotirmoy Barman <jyotirmoydotdev@gmail.com>
2026-07-24 20:26:14 +0530

    feat(api/healthcheck): return healthcheck as JSON with content type

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,8 +5,11 @@ import (
 	"net/http"
 )
 
-func (app *application) healcheckHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintln(w, "status: availble")
-	fmt.Fprintf(w, "enviroment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+func (app *application) healcheckHandler(w http.ResponseWriter, r *http.Request) {
+	js := `{"status": "availble", "enviroment": %q , "version": %q }`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte(js))
 }
