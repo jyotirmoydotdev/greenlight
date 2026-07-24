22f6bc35167c86d5313039697f9478dc59e061fd
Jyotirmoy Barman <jyotirmoydotdev@gmail.com>
2026-07-24 10:09:46 +0530

    style: align whitespace and formatting in cmd/api/main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,7 +13,7 @@ const version = "1.0.0"
 
 type config struct {
 	port int
-	env string
+	env  string
 }
 
 type application struct {
@@ -21,26 +21,24 @@ type application struct {
 	logger *log.Logger
 }
 
-func main(){
+func main() {
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.Parse()
 
-	logger := log.New(os.Stdout,"", log.Ldate | log.Ltime)
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	app := &application{
 		config: cfg,
 		logger: logger,
 	}
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healcheckHandler)
 
 	srv := &http.Server{
-		Addr: fmt.Sprintf(":%d", cfg.port),
-		Handler: app.routes(),
-		IdleTimeout: time.Minute,
-		ReadTimeout: 10 * time.Second,
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
