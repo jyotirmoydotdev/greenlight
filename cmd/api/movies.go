101bd8727cde0b47fc30bf5cb1284e924b5f2140
Jyotirmoy Barman <jyotirmoydotdev@gmail.com>
2026-07-24 10:04:13 +0530

    refactor(api): extract ID parsing helper and update movie handlers

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,20 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request){
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "create a new movie\n")
 }
 
-func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request){
-	params := httprouter.ParamsFromContext(r.Context())
-	id, err := strconv.ParseInt(params.ByName("id"),10,64)
-	if err != nil || id < 1{
-		http.NotFound(w,r)
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
 		return
 	}
 	fmt.Fprintf(w, "show the details of movie %d\n", id)
